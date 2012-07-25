c4e614b535338efe2658cb90047aaf54850561c1
André Santos <andrefs@cpan.org>
2012-07-25 10:51:09 +0100

    HOW MUCH IS THE FISH? (cenas para isto funcionar no naruto)

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 require 'sass'
 require 'mixpanel'
 
+# require_relative does not exist in ruby 1.8.7
+# This is a fallback -- http://stackoverflow.com/a/4718414/951432
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require './helpers/helpers.rb'
 
 ### Set Mixpanel
