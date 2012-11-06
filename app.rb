28b3260971249f3dea490f1b87f0ceac2135c2c8
Gabriel Poça <gabrielpoca@gmail.com>
2012-11-06 11:20:39 +0000

    v2 awesome

--- app.rb
+++ app.rb
@@ -4,20 +4,20 @@ require 'sass'
 require './helpers/helpers.rb'
 
 configure do
-  set :sass, :style => :compressed
+	set :sass, :style => :compressed
 end
 
 get '/stylesheets/:filename.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  filename = "#{params[:filename]}"
-  render :scss, filename.to_sym, :views => './views/stylesheets'
+	content_type 'text/css', :charset => 'utf-8'
+	filename = "#{params[:filename]}"
+	render :scss, filename.to_sym, :views => './views/stylesheets'
 end
 
 get '/' do
-  @javascripts = ['/javascripts/jquery.js']
-  erb :index
+	@javascripts = ['/javascripts/jquery.js', '/javascripts/app.js']
+	erb :index
 end
 
 post '/newsletter' do
-  redirect to('/') unless request.xhr?
+	redirect to('/') unless request.xhr?
 end
